f63fe6de053f3f8fe1bea0b921042a6bfe5918cd
runrungift <nmitsuki.g@gmail.com>
2025-02-08 14:56:25 +0900

    2

---
+++ forge-1.20.2-48.1.0/src/main/java/com/Light/CopperTools/main/CopperTools.java
@@ -0,0 +1,15 @@
+package com.Light.CopperTools.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("coppertools")
+//@Mod("半角英字のみ")
+
+public class CopperTools {
+    // MOD_IDの定義
+    public static final String MOD_ID = "coppertools";
+
+    public CopperTools(){
+
+    }
+}
